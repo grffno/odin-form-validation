9e8b6404356f853746ddb474594c9072e5a6a014
grffno <46036394+grffno@users.noreply.github.com>
2024-01-29 16:08:28 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Form Validation Practice",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
+  },
+};
